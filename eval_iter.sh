dcf6b254430d549ecf977ef17a4a23101371faf2
“zixuan” <chenzixuan0613@gmail.com>
2024-10-16 10:19:27 +0800

    init

---
+++ eval_iter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PATH=/opt/conda/bin:$PATH &&\
+cd /data/sjwlab/chenzx/code/MitoEM2021-Challenge/ &&\   # You can change the root path for your PC
+python connectomics/utils/evaluation/iteration_eval.py
+
+
+
